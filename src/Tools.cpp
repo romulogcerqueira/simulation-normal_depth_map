7609c9d5c6a36be497fd3360192dfa7a8fa304e3
Rômulo Cerqueira <romulogcerqueira@gmail.com>
2017-09-28 00:39:47 -0300

    good coding practice when the attenuation coefficient is calculated

--- src/Tools.cpp
+++ src/Tools.cpp
@@ -10,19 +10,21 @@ double underwaterSignalAttenuation( const double frequency,
                                     const double salinity,
                                     const double acidity) {
 
+    double frequency2 = frequency * frequency;
+
     // borid acid and magnesium sulphate relaxation frequencies (in kHz)
     double f1 = 0.78 * pow(salinity / 35, 0.5) * exp(temperature / 26);
     double f2 = 42 * exp(temperature / 17);
 
     // borid acid contribution
-    double borid = 0.106 * ((f1 * frequency * frequency) / (frequency * frequency + f1 * f1)) * exp((acidity - 8) / 0.56);
+    double borid = 0.106 * ((f1 * frequency2) / (frequency2 + f1 * f1)) * exp((acidity - 8) / 0.56);
 
     // magnesium sulphate contribuion
     double magnesium = 0.52 * (1 + temperature / 43) * (salinity / 35)
-                        * ((f2 * frequency * frequency) / (frequency * frequency + f2 * f2)) * exp(-depth / 6000);
+                        * ((f2 * frequency2) / (frequency2 + f2 * f2)) * exp(-depth / 6000);
 
     // freshwater contribution
-    double freshwater = 0.00049 * frequency * frequency * exp(-(temperature / 27 + depth / 17000));
+    double freshwater = 0.00049 * frequency2 * exp(-(temperature / 27 + depth / 17000));
 
     // absorptium attenuation coefficient in dB/km
     double attenuation = borid + magnesium + freshwater;
